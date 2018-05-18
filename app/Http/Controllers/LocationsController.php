7a8732c31474d79a29692a4f4f3faa3af31a4e9f
Steve Trap <trap.steve@hotmail.com>
2018-05-18 10:54:57 +0200

    fixed id error for sensors

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -40,10 +40,9 @@ class LocationsController extends Controller
 
         foreach($sensor_types as $sensor_type){
             $sensorId = sensors::where('name' , $sensor_type)->value('id');
-            var_dump($sensorId);
-            //$measurements[$sensor_type] = measurements::where('sensor_id', $sensorId)->get();
+            $measurements[$sensor_type] = measurements::where('sensor_id', $sensorId)->get();
           }
-          //return $measurements;
+          return $measurements;
     }
 
 }
